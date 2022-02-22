d52f8a5de1e1d6a701d93434acabb9969a1f4861
Wojciech Dyga <wojtek.dyga2@wp.pl>
2022-02-22 23:34:31 +0100

    Add next function in js (count, clearResult)

--- js/script.js
+++ js/script.js
@@ -15,13 +15,88 @@ let previousOperand = '';
 let actualOperand = '';
 let operation = undefined;
 
-//funkcje
+//FUNKCJE
+
+//obliczanie wyniku (sprwadzenie)
+
+const count = () => {
+    let action 
+    if(!previousOperand || !actualOperand){
+        return
+    }
+    const previous = parseFloat(previousOperand)
+    const actual = parseFloat(actualOperand)
+
+    if(isNaN(previous) || isNaN(actual)) {
+        return
+      }
+
+    switch (operation) {
+
+          case '+':
+          action = previous + actual
+          break
+
+          case '-':
+          action = previous - actual
+          break
+
+          case '×':
+          action = previous * actual
+          break
+
+          case '÷':
+          if(actual === 0){
+            alert('Numbers cannot be divided by 0!')
+            clearResult()
+            return
+          } 
+          action = previous / actual 
+          break
+
+          case '^':
+          if(previous.toString().includes('.')){
+            alert('You can only raise natural numbers')
+            clearResult()
+            return
+          } 
+            action = Math.pow(previous, actual)
+          break
+
+          case '!':
+          if(previous.toString().includes('.')){
+            alert('You can only use natural numbers')
+            clearResult()
+            return
+          }
+          if (previous === 1 || previous === 0 ) {
+            action = '1'
+          }
+            action = Math.pow(previous, actual)
+          break
+
+          default:
+          return
+    }
+actualOperand = action
+operation = undefined
+previousOperand = ''
+
+}
 
 // wybór danego operatora (jeśli jest dodana liczba wcześniej)
 
 const selectOperation = (sign) => {
     if(actualOperand === '') {
         return
+    }
+    if (previousOperand !== '') {
+        const previous = previousAction.innerText
+        if(actualOperand.toString === '0' && previous[previous.length - 1] === '÷'){
+            clearResult()
+            return
+        }
+        count ()
     }
     operation = sign
     previousOperand = actualOperand
@@ -56,6 +131,16 @@ const removeNumber = () => {
     actualOperand = actualOperand.toString().slice(0, -1)
 }
 
+// usuwanie wyniku
+
+const clearResult = () => {
+    previousOperand = '';
+    actualOperand = '';
+    operation = undefined;
+}
+
+
+
 
 
 //pętla która po kliknięciu w liczbę dodaje ją do ekranu
@@ -81,4 +166,14 @@ signs.forEach((sign) => {
         selectOperation(sign.innerText)
         updateResult()
     })
+})
+
+equal.addEventListener('click', () => {
+    count()
+    updateResult()
+})
+
+clear.addEventListener('click', () => {
+    clearResult()
+    updateResult()
 })
